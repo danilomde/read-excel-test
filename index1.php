3e8828e143c3fa61d5f653a3ad37324f1a7ef648
Tiago França <dev@tiagofranca.com>
2021-03-25 06:01:06 -0300

    novas alterações - No ramo modificacoes_tiago Mudanças a serem submetidas: 	new file:   .gitignore 	modified:   files/10.xlsx 	new file:   files/produtos.xlsx 	modified:   index1.php

--- index1.php
+++ index1.php
@@ -1,43 +1,69 @@
 <?php
-//  echo "<script>console.time('loading');</script>";
+require __DIR__.'/vendor/autoload.php';
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
 function dd($dump_and_die)
 {
     die(print_r($dump_and_die));
 }
 
-require 'vendor/autoload.php';
+function methods($instance)
+{
+    $methods = get_class_methods($instance); asort($methods); $methods = array_values($methods);
+    return $methods;
+}
 
-$inputFileType = 'Xlsx';
+function isValidColumn(array $row_data, array $required_columns)
+{
+    $all_valid = [];
+    foreach ($row_data as $k => $col)
+    {
+        if(in_array($k, array_values($required_columns)) && !empty($col))
+            $all_valid[] = $k;
+    }
+
+    return (count($all_valid) === count($required_columns)) ? true : false;
+}
+
 // $inputFileName = 'files/1000.xlsx';
-$inputFileName = 'files/10.xlsx';
+$inputFileName = 'files/produtos.xlsx';
 
-/*
-    // $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($inputFileType);
-    // $worksheetData = $reader->listWorksheetInfo($inputFileName);
+/**  Identify the type of $inputFileName  **/
+$inputFileType = IOFactory::identify($inputFileName); // $inputFileType = 'Xlsx';
 
-    // echo '<h3>Worksheet Information</h3>';
-    // echo '<ol>';
-    // foreach ($worksheetData as $worksheet) {
-    //     echo '<li>', $worksheet['worksheetName'], '<br />';
-    //     echo 'Rows: ', $worksheet['totalRows'],
-    //     ' Columns: ', $worksheet['totalColumns'], '<br />';
-    //     echo 'Cell Range: A1:',
-    //     $worksheet['lastColumnLetter'], $worksheet['totalRows'];
-    //     echo '</li>';
-    // }
-    // echo '</ol>';
-*/ 
 
 /**  Create a new Reader of the type defined in $inputFileType  **/
-$reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($inputFileType);
+$reader = IOFactory::createReader($inputFileType);
 /**  Advise the Reader that we only want to load cell data  **/
 $reader->setReadDataOnly(true);
 /**  Load $inputFileName to a Spreadsheet Object  **/
-$spreadsheet = $reader->load($inputFileName);
+$spreadsheet    = $reader->load($inputFileName);
+$init_sheet     = $spreadsheet->getSheetByName('produtos');
 
-// echo "<pre>";
-dd($spreadsheet->getCells());
-// echo "</pre>";
-
-// echo "<script>console.timeEnd('loading');</script>";
+if($init_sheet)
+{
+    $sheet = $init_sheet->rangeToArray(
+        'A2:R20',
+        NULL,
+        true,
+        true,
+        true
+    );
+    
+    # INICIO CORRETO
+    
+    $required_columns = [
+        'A',
+        'B',
+    ];
+    
+    foreach($sheet as $_row)
+    {
+        $is_valid = isValidColumn($_row, $required_columns);
+    
+        if($is_valid)
+            print_r($_row);
+    }
+    
+    dd(gettype($sheet));
+}
